de442c7b5dba5c49331229231aec877826921bf2
Bruno Martins <bgcngm@gmail.com>
2024-03-11 17:23:49 +0100

    sm8350-common: Use common libqti-perfd-client and power-libperfmgr

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -6,6 +6,8 @@
 
 COMMON_PATH := device/xiaomi/sm8350-common
 
+BOARD_VENDOR := xiaomi
+
 # A/B
 AB_OTA_PARTITIONS += \
     boot \
@@ -163,6 +165,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 VENDOR_SECURITY_PATCH := 2024-11-01
 
 # Sepolicy
+include device/lineage/sepolicy/libperfmgr/sepolicy.mk
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
