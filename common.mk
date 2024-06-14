4ce7552e10f7a11224cb8ebbab42362b82a0c27b
Yumi Yukimura <me.cafebabe@gmail.com>
2024-06-14 23:52:51 +0800

    sm8350-common: Setup DeviceAsWebcam
    Change-Id: Icf8e10cd21c4eef9a66af624971ffe9c4d39d992

--- common.mk
+++ common.mk
@@ -339,6 +339,7 @@ PRODUCT_PACKAGES += \
     TelephonyOverlaySM8350
 
 PRODUCT_PACKAGES += \
+    DeviceAsWebcamOverlaySM8350 \
     FrameworkOverlaySM8350 \
     NcmTetheringOverlay \
     SettingsOverlaySM8350 \
