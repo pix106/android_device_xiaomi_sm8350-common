e8e40abef2d085e65bebdaa9d4230ee571a05052
RobertGarciaa <chae0218@naver.com>
2024-10-31 20:27:50 +0900

    sm8350-common: Define missing HALs in FCM
    The following HALs in device manifest are not declared in FCM <= level 6:
       android.hardware.biometrics.fingerprint.IFingerprint/default (@4)
       android.hardware.bluetooth.audio.IBluetoothAudioProviderFactory/default (@4)
       android.hardware.drm.IDrmFactory/clearkey (@1)
       android.hardware.health.IHealth/default (@3)
       android.hardware.ir.IConsumerIr/default (@1)
       android.hardware.power.IPower/default (@5)
       android.hardware.sensors.ISensors/default (@2)
       android.hardware.thermal.IThermal/default (@1)
       android.hardware.usb.IUsb/default (@1)
       android.hardware.wifi.IWifi/default (@2)
       android.hardware.wifi.hostapd.IHostapd/default (@2)
       android.hardware.wifi.supplicant.ISupplicant/default (@3)
    COMPATIBLE

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -66,6 +66,7 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
     vendor/lineage/config/device_framework_matrix.xml \
+    $(COMMON_PATH)/hidl/vendor_framework_compatibility_matrix.xml \
     $(COMMON_PATH)/hidl/xiaomi_framework_compatibility_matrix.xml
 
 DEVICE_MANIFEST_FILE := \
