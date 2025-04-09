1d69f4c1731cf577df6c2590fd53b6fb97e7e0c0
pix106 <sbordenave@gmail.com>
2025-04-09 19:36:13 +0200

    Revert "sm8350-common: switch to common QCOM AIDL bootctrl HAL"
    This reverts commit eb3c37ade217f63c17d5e49c93545cd6e06b193a.

--- common.mk
+++ common.mk
@@ -108,8 +108,9 @@ TARGET_SCREEN_WIDTH := 1080
 
 # Boot control
 PRODUCT_PACKAGES += \
-    android.hardware.boot-service.qti \
-    android.hardware.boot-service.qti.recovery
+    android.hardware.boot@1.2-impl-qti \
+    android.hardware.boot@1.2-impl-qti.recovery \
+    android.hardware.boot@1.2-service
 
 # Camera
 PRODUCT_PACKAGES += \
