8cbb634c6cabdaa25e0803e42c817628e12b053c
Dhina17 <dhinalogu@gmail.com>
2025-04-12 15:10:39 +0200

    sm8350-common: camera: Implement torch light control
    [RealJohnGalt: Modify for oneplus usage and add toggle switch]
    [cyberknight777/ralph950412: Adapt nodes and SEPolicy for Xiaomi SM8350]

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -51,6 +51,9 @@ TARGET_PROVIDES_AUDIO_EXTNS := true
 # Bootloader
 TARGET_NO_BOOTLOADER := true
 
+# Camera
+TARGET_CAMERA_SERVICE_EXT_LIB := //$(COMMON_PATH):libcameraservice_extension.xiaomi_sm8350
+
 # Display
 TARGET_SCREEN_DENSITY ?= 440
 
