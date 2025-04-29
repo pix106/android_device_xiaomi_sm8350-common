4cb6b596d570fc7632ec7b6cbd7f447e037c3a75
lemezohaib <zhbkhan644@gmail.com>
2025-04-29 12:36:52 +0200

    sm8350-common: Move libcameraservice extension lib to select()
    Signed-off-by: lemezohaib <zhbkhan644@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -51,9 +51,6 @@ TARGET_PROVIDES_AUDIO_EXTNS := true
 # Bootloader
 TARGET_NO_BOOTLOADER := true
 
-# Camera
-TARGET_CAMERA_SERVICE_EXT_LIB := //$(COMMON_PATH):libcameraservice_extension.xiaomi_sm8350
-
 # Display
 TARGET_SCREEN_DENSITY ?= 440
 
