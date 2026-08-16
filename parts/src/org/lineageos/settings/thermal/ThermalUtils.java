4188d37a0cabcd43ba404f5c1ea8079fa91e9045
pix106 <sbordenave@gmail.com>
2026-08-16 17:02:06 +0200

    sm8350-common: parts: Fix build on cp2a

--- parts/src/org/lineageos/settings/thermal/ThermalUtils.java
+++ parts/src/org/lineageos/settings/thermal/ThermalUtils.java
@@ -27,7 +27,7 @@ import android.os.RemoteException;
 import android.os.SystemProperties;
 import android.os.UserHandle;
 import android.provider.MediaStore;
-import android.telecom.DefaultDialerManager;
+import android.telecom.TelecomManager;
 import android.view.Display;
 import android.view.Surface;
 import android.view.WindowManager;
@@ -220,7 +220,7 @@ public final class ThermalUtils {
 
         if (AppUtils.isBrowserApp(mContext, packageName, UserHandle.myUserId())) {
             return STATE_BROWSER;
-        } else if (DefaultDialerManager.getDefaultDialerApplication(mContext).equals(packageName)) {
+        } else if (packageName.equals(mContext.getSystemService(TelecomManager.class).getDefaultDialerPackage())) {
             return STATE_DIALER;
         } else if (isCameraApp(packageName)) {
             return STATE_CAMERA;
